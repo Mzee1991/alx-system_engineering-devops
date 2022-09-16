a4e6ddae6a0ea47102d4ee3d77ff1f0dd7cba29c
Mzee1991 <yohanamariamzee@gmail.com>
2022-09-16 13:30:49 +0300

    correction to pip3 package command

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,9 @@
 # installs the package flask
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+package { 'pip3':
+  ensure   => 'installed'
+}
+
+exec { 'pip install Flask':
+	command => '/usr/bin/pip3 install Flask',
+	version => '2.1.0'
 }
